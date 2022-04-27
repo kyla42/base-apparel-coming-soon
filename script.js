a95739f4a37eb81a02ba173904d097abdafb2108
kyla42 <103237579+kyla42@users.noreply.github.com>
2022-04-27 17:28:22 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const btn = document.getElementById("btn");
+const email = document.getElementById("email");
+const error = document.getElementById("error");
+const msg = document.getElementById("msg");
+
+const inputArea = document.getElementById("inputArea");
+
+btn.addEventListener("click", function () {
+    if (email.value.includes("@") && email.value.includes(".")) {
+        email.value = "";
+        msg.classList.remove("invalid");
+        email.classList.remove("invalid");
+        error.classList.remove("invalid");
+        window.location.href = "https://www.google.com/"
+    }
+    else {
+        msg.classList.add("invalid");
+        email.classList.add("invalid");
+        error.classList.add("invalid");
+    }
+});
